4e9a3eb89eb5e92aa7dd77dcf90d3fcf7319bd34
JonLedo <jonledo.code@gmail.com>
2023-02-01 11:22:21 -0500

    user passwords now hashed, updated User model validation (removed the max length)

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,8 +1,23 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
+// const { BadRequestError } = require('../errors')
+const bcrypt = require('bcryptjs')
 
 const register = async (req, res) => {
-  const user = await User.create({ ...req.body })
+  // create a temporary user object for bcrypt
+  // deconstruct these values from the body
+  const { name, email, password } = req.body
+
+  // hashed password
+  // create the "salt"
+  const salt = await bcrypt.genSalt(10)
+  // Generate the hashed password using salt and previous password string
+  const hashedPassword = await bcrypt.hash(password, salt)
+
+  // new user object w/ hashed password to be used in create function
+  const tempUser = { name, email, password: hashedPassword }
+
+  const user = await User.create({ ...tempUser })
   res.status(StatusCodes.CREATED).json({ user })
 }
 

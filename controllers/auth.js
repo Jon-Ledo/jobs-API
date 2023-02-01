6c133613038de9afe78ff74e542ccc22f9c10895
JonLedo <jonledo.code@gmail.com>
2023-02-01 14:12:51 -0500

    jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,24 +1,28 @@
 const User = require('../models/User')
 const { StatusCodes } = require('http-status-codes')
-// const { BadRequestError } = require('../errors')
-const bcrypt = require('bcryptjs')
 
 const register = async (req, res) => {
-  // create a temporary user object for bcrypt
-  // deconstruct these values from the body
-  const { name, email, password } = req.body
+  // // create a temporary user object for bcrypt
+  // // deconstruct these values from the body
+  // const { name, email, password } = req.body
 
-  // hashed password
-  // create the "salt"
-  const salt = await bcrypt.genSalt(10)
-  // Generate the hashed password using salt and previous password string
-  const hashedPassword = await bcrypt.hash(password, salt)
+  // // hashed password
+  // // create the "salt"
+  // const salt = await bcrypt.genSalt(10)
+  // // Generate the hashed password using salt and previous password string
+  // const hashedPassword = await bcrypt.hash(password, salt)
 
-  // new user object w/ hashed password to be used in create function
-  const tempUser = { name, email, password: hashedPassword }
+  // // new user object w/ hashed password to be used in create function
+  // const tempUser = { name, email, password: hashedPassword }
+  // const user = await User.create({ ...tempUser })
+  // ☝ the slightly loner way to do this. Refer to the User model to see how bcrypt is being used there
 
-  const user = await User.create({ ...tempUser })
-  res.status(StatusCodes.CREATED).json({ user })
+  const user = await User.create({ ...req.body })
+
+  // send both the token and user name fr front
+  const token = user.createJWT()
+
+  res.status(StatusCodes.CREATED).json({ user: { name: user.name }, token })
 }
 
 const login = async (req, res) => {
